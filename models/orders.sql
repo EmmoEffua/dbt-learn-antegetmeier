c7d0f0d786a08d2ea72767ceea43a9bc8ad85eaa
ansgar.tegetmeier@hdi.de <ansgar.tegetmeier@hdi.de>
2021-06-25 13:33:22 +0000

    played around

--- models/orders.sql
+++ models/orders.sql
@@ -1,24 +1,24 @@
 with 
-  payments as (
+  stg_orders as (
+    select customer_id, order_id  
+    from {{ ref('stg_orders')}}
+) 
+, stg_payments as (
     select 
-          order_id
+          payment_order_id
         , sum(payment_amount) as payment_amount  
     from {{ ref('stg_payments') }} 
     where payment_status <> 'fail'
-    group by order_id
-)
-, stg_orders as (
-    select customer_id, order_id  
-    from {{ ref('stg_orders')}}
+    group by payment_order_id
 )    
 , final as (
     select 
-          pm.order_id
+          od.order_id
         , od.customer_id
         , pm.payment_amount  
     from stg_orders as od
-    left outer join payments as pm 
-    on od.order_id = pm.order_id 
+    left outer join stg_payments as pm 
+    on od.order_id = pm.payment_order_id 
 )
 
  select *
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,6 +1,6 @@
 select 
       id as payment_id
-    , orderid as order_id
+    , orderid as payment_order_id
     , paymentmethod as payment_method
     , status as payment_status
     , amount*0.01 as payment_amount
